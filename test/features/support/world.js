15bc0387c453b5701075ebdaabd6b50795c1de0d
Daniel M. Lipton <daniel@mlipton.com>
2012-12-21 17:26:13 -0500

    Updated test/features/support/world.js
     * Added a longer browser wait
     * Added, but commented out require( 'assert' ).  Not sure I'm going to
    use it yet.

--- test/features/support/world.js
+++ test/features/support/world.js
@@ -3,10 +3,15 @@
 var zombie = require('zombie');
 var World = function World(callback) {
   this.browser = new zombie.Browser(); // this.browser will be available in step definitions
+  this.browser.waitFor = 10 * 1000;
 
   this.visit = function(url, callback) {
-    this.browser.visit(url, callback);
+
+    this.browser.visit(url, callback );
+
   };
+
+  //this.assert = require("assert");
 
   callback(); // tell Cucumber we're finished and to use 'this' as the world instance
 };
